86a5d92524f0bd820c51eaf525cf14797deb06ef
renatobrg <rnt.lmborges@gmail.com>
2021-01-25 20:29:27 -0300

    pag internas e ajustes

--- js/script.js
+++ js/script.js
@@ -8,37 +8,11 @@ $('#down').on('click', function() {
 $('#slick-home').slick({
     autoplay: true,
     pauseOnHover: true,
-    prevArrow: '<button type="button" class="slick-prev">teste</button>',
-    nextArrow: '<button type="button" class="slick-next">teste</button>'
+    prevArrow: '<button type="button" class="slick-prev"><img src="../imgs/prev.png" /></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../imgs/next.png" /></button>'
 })
 
-
-// var menuHamburger = document.querySelector('.menu-hamburger')
-// var navMobLi = document.querySelectorAll('.mob-link')
-
-// MENU HAMBURGER
-//   function openCloseMenu() {
-//     if (!conteudoMenuMobile.classList.contains('hide')) {
-//       conteudoMenuMobile.classList.remove('fade-out')
-//       conteudoMenuMobile.classList.add('fade-in')
-//       menuHamburger.classList.remove('open')
-//       menuHamburger.classList.add('close')
-//     } else {
-//       conteudoMenuMobile.classList.remove('fade-in')
-//       conteudoMenuMobile.classList.add('fade-out')
-//       menuHamburger.classList.remove('close')
-//       menuHamburger.classList.add('open')
-//     }
-//   }
-
-//   menuHamburger.addEventListener('click', function() {
-//     conteudoMenuMobile.classList.toggle('hide')
-//     openCloseMenu()
-//   })
-
-//   navMobLi.forEach(link => 
-//     link.addEventListener('click', function(){
-//       conteudoMenuMobile.classList.toggle('hide')
-//       openCloseMenu()
-//     })
-//   )
+lightbox.option({
+    'wrapAround': false,
+    'albumLabel': "Imagem %1 de %2"
+})
