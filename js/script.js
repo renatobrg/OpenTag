976048455e2eb7a974b5b988215363a93a0c2d88
renatobrg <rnt.lmborges@gmail.com>
2021-01-25 23:15:47 -0300

    menu mobile e links header

--- js/script.js
+++ js/script.js
@@ -12,6 +12,31 @@ $('#slick-home').slick({
     nextArrow: '<button type="button" class="slick-next"><img src="../imgs/next.png" /></button>'
 })
 
+
+var menuMob = document.getElementById('menu-mob')
+var btnHamburger = document.getElementById('btn-toggle')
+
+btnHamburger.addEventListener('click', function () {
+    if (menuMob.classList.contains('hide')) {
+        menuMob.classList.remove('fade-out')
+        menuMob.classList.remove('hide')
+        menuMob.classList.add('fade-in')
+        menuMob.classList.add('show')
+        btnHamburger.classList.remove('open')
+        btnHamburger.classList.add('close')
+        btnHamburger.classList.add('fixed')
+    } else {
+        menuMob.classList.remove('fade-in')
+        menuMob.classList.remove('show')
+        menuMob.classList.add('fade-out')
+        menuMob.classList.add('hide')
+        btnHamburger.classList.add('open')
+        btnHamburger.classList.remove('close')
+        btnHamburger.classList.remove('fixed')
+    }
+})
+
+
 lightbox.option({
     'wrapAround': false,
     'albumLabel': "Imagem %1 de %2"
